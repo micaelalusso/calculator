ba3460b687f2b9895b5a984c047a3bb7ec0c4d6f
Micaela <micalusso@gmail.com>
2023-04-05 16:09:53 -0300

    fin-calculator

--- src/components/Pantalla.js
+++ src/components/Pantalla.js
@@ -1,7 +1,15 @@
+import '../stylesheets/Pantalla.css';
 
-
-const Pantalla = () => (
+const Pantalla = ({ input }) => (
   <div className='input'>
-    
+    {input}
   </div>
 );
+
+export default Pantalla;
+
+/* definir un componente con una función flecha:
+- primero, definirlo como constante para que no se le pueda reasignar otro valor.
+- nombre de componente
+- le asignamos la función flecha, con ella le decimos a JS que vamos a retornar el valor entre paréntesis. 
+el valor es la estructura del componente definida con JSX, tenemos el div que tiene la clase input, dentro del div reemplazamos el valor del props*/
